bbd0ca58e38d77142ebb20008dfdbf99eb814f0f
Agustin Calcagno <agustin@savioke.com>
2020-05-26 18:40:08 -0300

    navigation improved

--- catkin_ws/src/calqui/scripts/goto_server.py
+++ catkin_ws/src/calqui/scripts/goto_server.py
@@ -8,6 +8,7 @@ import math
 from calqui_msgs.msg import GoToPlaceAction
 from calqui_msgs.msg import GoToPlaceGoal
 from calqui_msgs.msg import GoToPlaceResult
+from calqui_msgs.msg import GoToPlaceFeedback
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Pose2D
 from nav_msgs.msg import Odometry
@@ -19,8 +20,10 @@ class GoToServer:
         self._as = actionlib.SimpleActionServer('/goto_place', 
                 GoToPlaceAction, execute_cb=self.on_goal, auto_start=False)
         self._vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        self._fb_pub = rospy.Publisher("/goto_place/feedback", GoToPlaceFeedback, queue_size=10)
         self._odom_sub = rospy.Subscriber("/odom", Odometry, self.on_odom)
         self._current_pose = Pose2D()
+        self._goal_pose = Pose2D()
         self._as.start()
         
         rospy.loginfo('Simple Action Server has been started')
@@ -38,7 +41,12 @@ class GoToServer:
         self._current_pose.x = msg.pose.pose.position.x
         self._current_pose.y = msg.pose.pose.position.y
         self._current_pose.theta = yaw
+        
         # rospy.loginfo('current pose is {}'.format(self._current_pose))
+        fb = GoToPlaceFeedback()
+        fb.distance_to_goal = self.distance_to_goal()
+        self._fb_pub.publish(fb)
+        
 
     def set_vel(self, linear_x, angular_z):
         rospy.loginfo('publishing vel')
@@ -54,39 +62,42 @@ class GoToServer:
     def distance_to_goal(self):
         return math.sqrt(math.pow((self._goal_pose.y - self._current_pose.y),2) + math.pow((self._goal_pose.x - self._current_pose.x),2))
 
-    def calculate_angular_speed(self):
+    # absolute angle to the goal
+    def angle_to_goal(self):
         # avoid division by 0
         if self._current_pose.x == 0:
-            angle_to_goal = 0 
+            return 0 
         else:
-            angle_to_goal = numpy.arctan((self._goal_pose.y - self._current_pose.y) / (self._goal_pose.x - self._current_pose.x))
-        
-        current_angle = self._current_pose.theta
+            return numpy.arctan2((self._goal_pose.y - self._current_pose.y), (self._goal_pose.x - self._current_pose.x))
 
-        if current_angle < angle_to_goal + 0.1 and current_angle > angle_to_goal - 0.1:
+    # how far are we of pointing to the goal?
+    def deviation(self):
+        return self._current_pose.theta - self.angle_to_goal()
+
+    # do we consider that we reached the goal
+    def goal_was_reached(self):
+        return self._current_pose and self._goal_pose and self.distance_to_goal() < 0.5
+
+    def calculate_angular_speed(self):
+        if numpy.abs(self.deviation()) < 0.1 or self.goal_was_reached():
             rotation_speed = 0
         else:
-            rotation_speed = 0.1
+            rotation_speed = -0.1 * self.deviation()
 
         rospy.loginfo('rotation_speed {}'.format(rotation_speed))
-        rospy.loginfo('angle_to_goal {}, current_angle {}'.format(angle_to_goal, current_angle))
+        rospy.loginfo('angle_to_goal {}, current_angle {}'.format(self.angle_to_goal(), self._current_pose.theta))
         return rotation_speed
 
     def calculate_linear_speed(self, angular_speed):
-        if angular_speed:
+        if self.goal_was_reached() or numpy.abs(self.deviation()) > 0.5:
             return 0
-        distnace_to_goal = self.distance_to_goal()
-        if distnace_to_goal < 0.5:
-            return 0
-        return -0.1
+        return 0.5
 
     def calculate_vel(self):
         # rospy.loginfo('current pose {} -> goal {}'.format(self._current_pose, self._goal_pose))
         msg = Twist()
-
         angular_speed = self.calculate_angular_speed()
         linear_speed = self.calculate_linear_speed(angular_speed)
-
         msg.linear.x = linear_speed
         msg.linear.y = 0
         msg.linear.z = 0
@@ -94,7 +105,6 @@ class GoToServer:
         msg.angular.y = 0
         msg.angular.z = angular_speed
         return msg
-
 
     def on_goal(self, goal):
         rospy.loginfo('a goal has been received! {}'.format(goal))
@@ -109,24 +119,20 @@ class GoToServer:
         wait_duration = 1
         rate = rospy.Rate(1.0/wait_duration)
 
-        self._count = 0
         while True:
-            self._count = self._count + 1
             vel = self.calculate_vel()
             self.set_vel(vel.linear.x, vel.angular.z)
             rate.sleep()
 
         self.set_vel(0.0, 0.0)
         result = GoToPlaceResult()
-        result.x = 1
-        result.y = 2
-        result.theta = 3
+        result.x = self._current_pose.x
+        result.y = self._current_pose.y
+        result.theta = self._current_pose.theta
 
         self._as.set_succeeded(result)
 
 if __name__ == '__main__':
         rospy.init_node('goto_place_server')
-
         server = GoToServer()
-
         rospy.spin()
